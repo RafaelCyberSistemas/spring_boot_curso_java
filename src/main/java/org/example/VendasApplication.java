1f422a68df60755f5c6864b517d6ecef41ecf46f
RafaelCyberSistemas <rafaelcybersistemas@gmail.com>
2021-10-11 13:17:36 -0300

    limpeza do projeto para o estudo do modulo de conexão com os BD

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,32 +1,13 @@
 package org.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@ComponentScan(basePackages =   {"com.outrabiblioteca.projeto",
-                                "org.example.repository",
-                                "org.example.service", "org.example"})
+
 @RestController
 public class VendasApplication {
-
-    //@Autowired
-    //@Qualifier("applicationName")
-    @Value("${application.name}")
-    private String applicationName;
-
+    
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String hello(){
-        return applicationName;
     }
 }
