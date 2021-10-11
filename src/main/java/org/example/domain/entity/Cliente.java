f7cb6dc02c73f6a374f07038f2ed94e103689646
RafaelCyberSistemas <rafaelcybersistemas@gmail.com>
2021-10-11 20:05:38 -0300

    implementação do repository com entitymanager

--- src/main/java/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -1,8 +1,16 @@
 package org.example.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente() {
