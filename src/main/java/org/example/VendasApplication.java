2fbf25d71639caac43e0e7707dce10b30d203010
RafaelCyberSistemas <rafaelcybersistemas@gmail.com>
2021-10-11 18:37:24 -0300

    repository implementado com JDBCTEMPLATE

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,12 +1,55 @@
 package org.example;
 
+import org.example.domain.entity.Cliente;
+import org.example.domain.entity.repositorio.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 
-@RestController
+@SpringBootApplication
 public class VendasApplication {
-    
+
+    @Bean
+    public CommandLineRunner init(@Autowired ClienteRepository clienteRepository){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clienteRepository.salvar(new Cliente("Rafael"));
+            clienteRepository.salvar(new Cliente("Outro Cliente qualquer"));
+
+            System.out.println("Listando todos os clientes");
+            List<Cliente> todosClientes = clienteRepository.obterTodosClientes();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c->{
+                c.setNome(c.getNome() + " atualizado");
+                clienteRepository.atualizar(c);
+            });
+            System.out.println("Buscando todos os  clientes atualizados");
+            todosClientes = clienteRepository.obterTodosClientes();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("buscando clientes por nome");
+            clienteRepository.buscarPorNome("Cli").forEach(System.out::println);
+
+            System.out.println("Deletando clientes");
+            clienteRepository.obterTodosClientes().forEach(c->{
+                clienteRepository.deletar(c);
+            });
+
+            todosClientes = clienteRepository.obterTodosClientes();
+            if(todosClientes.isEmpty()){
+                System.out.println("Todos os clientes foram deletados");
+            }else{
+                todosClientes.forEach(System.out::println);
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
